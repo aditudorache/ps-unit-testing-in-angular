28842b0fe1355a8651d233c5a720e412ed7c8e40
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-05-05 08:03:23 +0200

    chapter 4 and 5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
